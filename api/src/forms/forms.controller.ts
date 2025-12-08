ed8ffee28941bc6cd08135d60a3940ab9000df9b
Bismark-P <bismarkpinkrah@hotmail.com>
2025-12-08 13:07:28 +0000

    Form CRUD

--- api/src/forms/forms.controller.ts
+++ api/src/forms/forms.controller.ts
@@ -2,41 +2,70 @@ import {
   Controller,
   Get,
   Post,
-  Body,
   Patch,
-  Param,
   Delete,
+  Param,
+  Body,
+  Query,
+  UseGuards,
 } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { FormsService } from './forms.service';
 import { CreateFormDto } from './dto/create-form.dto';
 import { UpdateFormDto } from './dto/update-form.dto';
+import { AuthGuard } from '../common/guards/auth.guard';
 
+@ApiTags('forms')
 @Controller('forms')
 export class FormsController {
   constructor(private readonly formsService: FormsService) {}
 
+  @UseGuards(AuthGuard)
   @Post()
+  @ApiCreatedResponse({ description: 'The form has been successfully created.' })
+  @ApiBadRequestResponse({ description: 'Invalid input data provided.' })
   create(@Body() createFormDto: CreateFormDto) {
     return this.formsService.create(createFormDto);
   }
 
   @Get()
-  findAll() {
-    return this.formsService.findAll();
+  @ApiOkResponse({ description: 'Forms retrieved successfully.' })
+  findAll(@Query() { filter = '{}' }: { filter: string }) {
+    return this.formsService.findAll(JSON.parse(filter));
+  }
+
+  @Get('count')
+  countDocuments(@Query() { filter = '{}' }: { filter: string }) {
+    return this.formsService.countDocuments(JSON.parse(filter));
   }
 
   @Get(':id')
+  @ApiOkResponse({ description: 'The form has been successfully found.' })
+  @ApiNotFoundResponse({ description: 'Form with the given ID not found.' })
   findOne(@Param('id') id: string) {
-    return this.formsService.findOne(+id);
+    return this.formsService.findOne({ _id: id });
   }
 
+  @UseGuards(AuthGuard)
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFormDto: UpdateFormDto) {
-    return this.formsService.update(+id, updateFormDto);
+  @ApiOkResponse({ description: 'The form has been successfully updated.' })
+  @ApiNotFoundResponse({ description: 'Form with the given ID not found.' })
+  @ApiBadRequestResponse({ description: 'Invalid input data provided.' })
+  updateOne(@Param('id') id: string, @Body() updateFormDto: UpdateFormDto) {
+    return this.formsService.updateOne({ _id: id }, updateFormDto);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.formsService.remove(+id);
+  @ApiOkResponse({ description: 'The form has been successfully deleted.' })
+  @ApiNotFoundResponse({ description: 'Form with the given ID not found.' })
+  deleteOne(@Param('id') id: string) {
+    return this.formsService.deleteOne({ _id: id });
   }
 }
--- api/src/forms/forms.module.ts
+++ api/src/forms/forms.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
-import { FormsService } from './forms.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { FormsController } from './forms.controller';
+import { FormsService } from './forms.service';
+import { Form, FormSchema } from './schemas/form.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Form.name, schema: FormSchema },
+    ]),
+  ],
   controllers: [FormsController],
   providers: [FormsService],
 })
